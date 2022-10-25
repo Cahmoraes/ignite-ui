467381867f0af284d4028fb800fab061d29187e7
caique.moraes <caique.moraes@compasso.com.br>
2022-10-25 14:49:33 -0300

    feat: add core components

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,26 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data Display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/cahmoraes.png',
+    alt: 'Caique Moraes',
+  },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,36 @@
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+import { fonts } from '../../../tokens/src/fonts'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    lineHeights,
+    radii,
+    space,
+    fonts,
+  },
+})
